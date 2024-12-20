daa0aeba00e6f1ba97a52b81771fb97c6e6acb8b
Alim Mahmudov <alimmahmudov27@gmail.com>
2024-12-20 23:54:36 +0600

    details

--- src/appPages/components/pages/details/sections/details/Details.tsx
+++ src/appPages/components/pages/details/sections/details/Details.tsx
@@ -1,10 +1,22 @@
+"use client";
+import { useParams } from "next/navigation";
+import { UseData } from "../../../data/data";
 import scss from "./Details.module.scss";
 
 const Details = () => {
+  const { data } = UseData();
+  const { id } = useParams();
+
+  // Ensure id is converted to number for comparison
+  const findProduct = data.find((el) => el.id === Number(id));
+
   return (
     <div id={scss.Details}>
       <div className="container">
-        <div className={scss.details}>Details</div>
+        <div className={scss.details}>
+          <img src={findProduct?.image} alt="" />
+          <h1>{findProduct?.model}</h1>
+        </div>
       </div>
     </div>
   );
