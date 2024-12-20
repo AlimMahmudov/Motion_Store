fcceb68143341b9c7ec460531cd3a2388eda15e9
Alim Mahmudov <alimmahmudov27@gmail.com>
2024-12-21 03:21:13 +0600

    detail

--- src/appPages/components/pages/details/DetailsPage.tsx
+++ src/appPages/components/pages/details/DetailsPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Details from "./sections/details/Details";
+import Hits from "../home/sections/hits/Hits";
 
 const DetailsPage = () => {
   return (
     <div>
       <Details />
+      <Hits />
     </div>
   );
 };
--- src/appPages/components/pages/details/sections/details/Details.tsx
+++ src/appPages/components/pages/details/sections/details/Details.tsx
@@ -7,7 +7,6 @@ const Details = () => {
   const { data } = UseData();
   const { id } = useParams();
 
-  // Ensure id is converted to number for comparison
   const findProduct = data.find((el) => el.id === Number(id));
 
   return (
@@ -15,7 +14,35 @@ const Details = () => {
       <div className="container">
         <div className={scss.details}>
           <img src={findProduct?.image} alt="" />
-          <h1>{findProduct?.model}</h1>
+          <div className={scss.text}>
+            <h1>{findProduct?.model}</h1>
+            <p>{findProduct?.Description}</p>
+
+            <h3>Основные характеристики</h3>
+            <div className={scss.haracter}>
+              <div className={scss.haracter_text}>
+                <p>Модель:</p>
+                <p>Бренд:</p>
+                <p>Размер оперативной памяти:</p>
+                <p>Процессор:</p>
+                <p>Размер хранилища:</p>
+              </div>
+              <div className={scss.haracter_data}>
+                <p>{findProduct?.model}</p>
+                <p>{findProduct?.brand}</p>
+                <p>{findProduct?.Ram} ГБ</p>
+                <p>{findProduct?.processor}</p>
+                <p>{findProduct?.storage} ГБ</p>
+              </div>
+            </div>
+            <div className={scss.price}>
+              <h2>{findProduct?.price} сом</h2>
+              <div className={scss.buttons}>
+                <button>Купить</button>
+                <button className={scss.favorite}>O</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
