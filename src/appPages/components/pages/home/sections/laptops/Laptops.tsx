596a70226fa1279375cf5b8731b0b14b635960f6
Alim Mahmudov <alimmahmudov27@gmail.com>
2024-12-20 20:05:21 +0600

    sort

--- src/appPages/components/pages/home/sections/laptops/Laptops.tsx
+++ src/appPages/components/pages/home/sections/laptops/Laptops.tsx
@@ -6,103 +6,123 @@ import { MdFavoriteBorder } from "react-icons/md";
 const data = [
   {
     id: 1,
-    img: "https://www.ultra.kg/upload/resize_cache/iblock/adb/220_200_1/adb16efad468274b2783d48c7b3497f5.jpg",
-    name: "Acer",
-    price: "65 000",
-    ram: "16GB LPDDR5 6400MHz.",
-    ssd: "1024GB PCle NVMe",
-    display: "14.0”2.8K OLED Adobe & DCI-P3 100%",
-    cpu: "AMD Ryzen 7 7840U",
+    brand: "lenovo",
+    model: "lenovo laptop",
+    processor: "core i5",
+    Ram: "8",
+    storage: "256",
+    price: "28000",
+    Description: "супер ноутбук от Шейх байке",
+    image:
+      "https://www.ultra.kg/upload/resize_cache/iblock/adb/220_200_1/adb16efad468274b2783d48c7b3497f5.jpg",
   },
   {
     id: 2,
-    img: "https://object.pscloud.io/cms/cms/Photo/img_0_62_2967_0_1_DfX6J2.webp",
-    name: "Lenovo",
-    price: "65 000",
-    ram: "16GB LPDDR5 6400MHz.",
-    ssd: "1024GB PCle NVMe",
-    display: "14.0”2.8K OLED Adobe & DCI-P3 100%",
-    cpu: "AMD Ryzen 7 7840U",
+    brand: "acer",
+    model: "acer laptop",
+    processor: "core i5",
+    Ram: "8",
+    storage: "256",
+    price: "28000",
+    Description: "супер ноутбук от Шейх байке",
+    image:
+      "https://www.ultra.kg/upload/resize_cache/iblock/adb/220_200_1/adb16efad468274b2783d48c7b3497f5.jpg",
   },
   {
     id: 3,
-    img: "https://object.pscloud.io/cms/cms/Photo/img_0_62_3272_0_1_quuxOm.webp",
-    name: "HP",
-    price: "65 000",
-    ram: "16GB LPDDR5 6400MHz.",
-    ssd: "1024GB PCle NVMe",
-    display: "14.0”2.8K OLED Adobe & DCI-P3 100%",
-    cpu: "AMD Ryzen 7 7840U",
+    brand: "hp",
+    model: "hp laptop",
+    processor: "core i5",
+    Ram: "8",
+    storage: "256",
+    price: "28000",
+    Description: "супер ноутбук от Шейх байке",
+    image:
+      "https://www.ultra.kg/upload/resize_cache/iblock/adb/220_200_1/adb16efad468274b2783d48c7b3497f5.jpg",
   },
   {
     id: 4,
-    img: "https://www.ultra.kg/upload/resize_cache/iblock/adb/220_200_1/adb16efad468274b2783d48c7b3497f5.jpg",
-    name: "Acer2",
-    price: "65 000",
-    ram: "16GB LPDDR5 6400MHz.",
-    ssd: "1024GB PCle NVMe",
-    display: "14.0”2.8K OLED Adobe & DCI-P3 100%",
-    cpu: "AMD Ryzen 7 7840U",
+    brand: "dell",
+    model: "dell laptop",
+    processor: "core i5",
+    Ram: "8",
+    storage: "256",
+    price: "28000",
+    Description: "супер ноутбук от Шейх байке",
+    image:
+      "https://www.ultra.kg/upload/resize_cache/iblock/adb/220_200_1/adb16efad468274b2783d48c7b3497f5.jpg",
   },
   {
     id: 5,
-    img: "https://object.pscloud.io/cms/cms/Photo/img_0_62_2967_0_1_DfX6J2.webp",
-    name: "Lenovo2",
-    price: "65 000",
-    ram: "16GB LPDDR5 6400MHz.",
-    ssd: "1024GB PCle NVMe",
-    display: "14.0”2.8K OLED Adobe & DCI-P3 100%",
-    cpu: "AMD Ryzen 7 7840U",
+    brand: "asus",
+    model: "asus laptop",
+    processor: "core i5",
+    Ram: "8",
+    storage: "256",
+    price: "28000",
+    Description: "супер ноутбук от Шейх байке",
+    image:
+      "https://www.ultra.kg/upload/resize_cache/iblock/adb/220_200_1/adb16efad468274b2783d48c7b3497f5.jpg",
   },
   {
     id: 6,
-    img: "https://www.ultra.kg/upload/resize_cache/iblock/adb/220_200_1/adb16efad468274b2783d48c7b3497f5.jpg",
-    name: "Acer3",
-    price: "65 000",
-    ram: "16GB LPDDR5 6400MHz.",
-    ssd: "1024GB PCle NVMe",
-    display: "14.0”2.8K OLED Adobe & DCI-P3 100%",
-    cpu: "AMD Ryzen 7 7840U",
+    brand: "mac",
+    model: "mac laptop",
+    processor: "core i5",
+    Ram: "8",
+    storage: "256",
+    price: "28000",
+    Description: "супер ноутбук от Шейх байке",
+    image:
+      "https://www.ultra.kg/upload/resize_cache/iblock/adb/220_200_1/adb16efad468274b2783d48c7b3497f5.jpg",
   },
   {
     id: 7,
-    img: "https://object.pscloud.io/cms/cms/Photo/img_0_62_2967_0_1_DfX6J2.webp",
-    name: "Lenovo3",
-    price: "65 000",
-    ram: "16GB LPDDR5 6400MHz.",
-    ssd: "1024GB PCle NVMe",
-    display: "14.0”2.8K OLED Adobe & DCI-P3 100%",
-    cpu: "AMD Ryzen 7 7840U",
+    brand: "mac",
+    model: "mac m2 laptop",
+    processor: "core i5",
+    Ram: "8",
+    storage: "256",
+    price: "28000",
+    Description: "супер ноутбук от Шейх байке",
+    image:
+      "https://www.ultra.kg/upload/resize_cache/iblock/adb/220_200_1/adb16efad468274b2783d48c7b3497f5.jpg",
   },
   {
     id: 8,
-    img: "https://object.pscloud.io/cms/cms/Photo/img_0_62_3272_0_1_quuxOm.webp",
-    name: "HP2",
-    price: "65 000",
-    ram: "16GB LPDDR5 6400MHz.",
-    ssd: "1024GB PCle NVMe",
-    display: "14.0”2.8K OLED Adobe & DCI-P3 100%",
-    cpu: "AMD Ryzen 7 7840U",
+    brand: "mac",
+    model: "mac m3 laptop",
+    processor: "core i5",
+    Ram: "8",
+    storage: "256",
+    price: "28000",
+    Description: "супер ноутбук от Шейх байке",
+    image:
+      "https://www.ultra.kg/upload/resize_cache/iblock/adb/220_200_1/adb16efad468274b2783d48c7b3497f5.jpg",
   },
   {
     id: 9,
-    img: "https://www.ultra.kg/upload/resize_cache/iblock/adb/220_200_1/adb16efad468274b2783d48c7b3497f5.jpg",
-    name: "Acer4",
-    price: "65 000",
-    ram: "16GB LPDDR5 6400MHz.",
-    ssd: "1024GB PCle NVMe",
-    display: "14.0”2.8K OLED Adobe & DCI-P3 100%",
-    cpu: "AMD Ryzen 7 7840U",
+    brand: "lenovo",
+    model: "lenovo2 laptop",
+    processor: "core i5",
+    Ram: "8",
+    storage: "256",
+    price: "28000",
+    Description: "супер ноутбук от Шейх байке",
+    image:
+      "https://www.ultra.kg/upload/resize_cache/iblock/adb/220_200_1/adb16efad468274b2783d48c7b3497f5.jpg",
   },
   {
     id: 10,
-    img: "https://object.pscloud.io/cms/cms/Photo/img_0_62_2967_0_1_DfX6J2.webp",
-    name: "Lenovo4",
-    price: "65 000",
-    ram: "16GB LPDDR5 6400MHz.",
-    ssd: "1024GB PCle NVMe",
-    display: "14.0”2.8K OLED Adobe & DCI-P3 100%",
-    cpu: "AMD Ryzen 7 7840U",
+    brand: "acer",
+    model: "acer2 laptop",
+    processor: "core i5",
+    Ram: "8",
+    storage: "256",
+    price: "28000",
+    Description: "супер ноутбук от Шейх байке",
+    image:
+      "https://www.ultra.kg/upload/resize_cache/iblock/adb/220_200_1/adb16efad468274b2783d48c7b3497f5.jpg",
   },
 ];
 
@@ -112,6 +132,11 @@ const Laptops = () => {
     <div id={scss.Laptops}>
       <div className="container">
         <div className={scss.laptops}>
+          <div className={scss.sort}>
+            {data.map((item, index) => (
+              <button key={index}>{item.brand}</button>
+            ))}
+          </div>
           <div className={scss.block}>
             {data.map((el, index) => (
               <div
@@ -119,8 +144,8 @@ const Laptops = () => {
                 key={index}
                 className={scss.box}
               >
-                <img src={el.img} alt="" />
-                <h1>{el.name}</h1>
+                <img src={el.image} alt="" />
+                <h1>{el.model}</h1>
                 <div className={scss.price}>
                   <h2>{el.price} сом</h2>
                   <p>На витрине</p>
