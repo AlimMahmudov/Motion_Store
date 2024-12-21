0f8d66eae38eeb45c599a3a9c262afec721b40e3
Alim Mahmudov <alimmahmudov27@gmail.com>
2024-12-21 14:58:21 +0600

    адаптация

--- src/appPages/components/layout/header/Header.tsx
+++ src/appPages/components/layout/header/Header.tsx
@@ -10,42 +10,42 @@ import { GrCart } from "react-icons/gr";
 import Link from "next/link";
 
 const Header = () => {
-	const { searchQuery, setSearchQuery } = useSearchStore();
+  const { searchQuery, setSearchQuery } = useSearchStore();
 
-	return (
-		<header id={scss.Header}>
-			<div className="container">
-				<div className={scss.header}>
-					<Link href={"/"}>
-						<div className={scss.logo}>
-							<Image src={logo} alt="img" />
-							<h1>EduHub</h1>
-						</div>
-					</Link>
-					<Link href={"/search"}>
-						<input
-							type="text"
-							placeholder="Поиск..."
-							value={searchQuery}
-							onChange={(e) => setSearchQuery(e.target.value)}
-							className="search-input"
-						/>
-					</Link>
-					<div className={scss.content}>
-						<div className={scss.icons}>
-							<button>
-								<MdFavoriteBorder />
-							</button>
-							<button>
-								<GrCart />
-							</button>
-						</div>
-						<button>+996 773400551</button>
-					</div>
-				</div>
-			</div>
-		</header>
-	);
+  return (
+    <header id={scss.Header}>
+      <div className="container">
+        <div className={scss.header}>
+          <Link href={"/"}>
+            <div className={scss.logo}>
+              <Image src={logo} alt="img" />
+              <h1>EduHub</h1>
+            </div>
+          </Link>
+          <Link href={"/search"}>
+            <input
+              type="text"
+              placeholder="Поиск..."
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className="search-input"
+            />
+          </Link>
+          <div className={scss.content}>
+            <div className={scss.icons}>
+              <button>
+                <MdFavoriteBorder />
+              </button>
+              <button>
+                <GrCart />
+              </button>
+            </div>
+            <button>+996 773400551</button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/appPages/components/pages/home/HomePage.tsx
+++ src/appPages/components/pages/home/HomePage.tsx
@@ -9,7 +9,7 @@ const HomePage = () => {
   return (
     <div>
       <Home />
-      <Sort/>
+      <Sort />
       <Laptops />
       <Hits />
       <Contact />
