09bb6c13d795df2a7ea0e546de124a00f89b14b8
Alim Mahmudov <alimmahmudov27@gmail.com>
2025-01-08 12:08:31 +0600

    basket reset

--- src/appPages/components/pages/basketPage/Basket.tsx
+++ src/appPages/components/pages/basketPage/Basket.tsx
@@ -8,138 +8,155 @@ import axios from "axios";
 
 // Тип для элемента корзины
 type BasketItem = {
-	id: number;
-	model: string;
-	brand: string;
-	price: number;
-	quantity: number;
-	photos?: { image: string }[];
-	addedAt: string;
+  id: number;
+  model: string;
+  brand: string;
+  price: number;
+  quantity: number;
+  photos?: { image: string }[];
+  addedAt: string;
 };
 
 type IFormTelegram = {
-	phone: string;
-	email: string;
-	basket: [BasketItem];
+  phone: string;
+  email: string;
+  basket: [BasketItem];
 };
 
 const Basket = () => {
-	const { basket, addToBasket, deleteOneBasket, removeFromBasket } =
-		useBasketStore();
-	const { register, handleSubmit, reset } = useForm<IFormTelegram>();
+  const {
+    basket,
+    addToBasket,
+    deleteOneBasket,
+    removeFromBasket,
+    clearBasket,
+  } = useBasketStore();
+  const { register, handleSubmit, reset } = useForm<IFormTelegram>();
 
-	const messageModel = (data: IFormTelegram) => {
-		let messageTG = `<b>Product Order</b>\n`;
-		basket.forEach((item) => {
-			messageTG += `\nName: <b>${item.model}</b>\n`;
-			messageTG += `Description: ${item.brand}\n`;
-			messageTG += `Price: ${Number(item.price)} x ${Number(item.quantity)} = ${
-				Number(item.price) * Number(item.quantity)
-			} сом\n`;
-		});
-		messageTG += `\nPhone: ${data.phone}\n`;
-		messageTG += `Email: ${data.email}\n`;
-		return messageTG;
-	};
+  const messageModel = (data: IFormTelegram) => {
+    let messageTG = `<b>Product Order</b>\n`;
+    basket.forEach((item) => {
+      messageTG += `\nName: <b>${item.model}</b>\n`;
+      messageTG += `Description: ${item.brand}\n`;
+      messageTG += `Price: ${Number(item.price)} x ${Number(item.quantity)} = ${
+        Number(item.price) * Number(item.quantity)
+      } сом\n`;
+    });
+    messageTG += `\nPhone: ${data.phone}\n`;
+    messageTG += `Email: ${data.email}\n`;
+    return messageTG;
+  };
 
-	const onSubmit: SubmitHandler<IFormTelegram> = async (data) => {
-		try {
-			await axios.post(
-				`https://api.telegram.org/bot${"7350084863:AAGNHWJpQ7qif2WAAinzTBqVX3nwE-0sgbk"}/sendMessage`,
-				{
-					chat_id: -1002178370559,
-					parse_mode: "html",
-					text: messageModel(data),
-				}
-			);
-			alert("Данные успешно отправлены!");
-			reset();
-		} catch (error) {
-			console.error("Ошибка при отправке данных в Telegram:", error);
-			alert("Произошла ошибка при отправке. Попробуйте позже.");
-		}
-	};
+  const onSubmit: SubmitHandler<IFormTelegram> = async (data) => {
+    try {
+      await axios.post(
+        `https://api.telegram.org/bot${"7350084863:AAGNHWJpQ7qif2WAAinzTBqVX3nwE-0sgbk"}/sendMessage`,
+        {
+          chat_id: -1002178370559,
+          parse_mode: "html",
+          text: messageModel(data),
+        }
+      );
+      alert("Данные успешно отправлены!");
+      clearBasket(); // Очищаем корзину
+      reset(); // Сбрасываем форму
+    } catch (error) {
+      console.error("Ошибка при отправке данных в Telegram:", error);
+      alert("Произошла ошибка при отправке. Попробуйте позже.");
+    }
+  };
 
-	return (
-		<div id={scss.Basket}>
-			<div className="container">
-				{basket.length === 0 ? (
-					<div className={scss.emptyBasket}>
-						<h1>Корзина пуста</h1>
-						<p>
-							Загляните на главную, чтобы выбрать товары или найдите нужное в
-							поиске
-						</p>
-						<button>
-							<Link href="/">Перейти на главную</Link>
-						</button>
-					</div>
-				) : (
-					<>
-						<div className={scss.desc}>
-							<p>картинка</p>
-							<p>название</p>
-							<p>цена</p>
-							<p>количество</p>
-							<p>удалить</p>
-						</div>
-						<div className={scss.content}>
-							{basket.map((el) => (
-								<div key={el.id} className={scss.product}>
-									<div className={scss.box_img}>
-										{el.photos?.map(
-											(item, index) =>
-												index === 0 && (
-													<Image
-														key={index}
-														width={250}
-														height={200}
-														src={item.image}
-														alt="img"
-													/>
-												)
-										)}
-									</div>
-									<div className={scss.details}>
-										<h2 className={scss.model}>Модель: {el.model}</h2>
-										<h3 className={scss.price}>
-											Цена: {Number(el.price) * Number(el.quantity)} сом
-										</h3>
-										<div className={scss.quantity}>
-											<button
-												onClick={() => deleteOneBasket(el)}
-												className={scss.decrease}>
-												-
-											</button>
-											<span>{el.quantity}</span>
-											<button
-												onClick={() => addToBasket(el)}
-												className={scss.increase}>
-												+
-											</button>
-										</div>
+  return (
+    <div id={scss.Basket}>
+      <div className="container">
+        {basket.length === 0 ? (
+          <div className={scss.emptyBasket}>
+            <h1>Корзина пуста</h1>
+            <p>
+              Загляните на главную, чтобы выбрать товары или найдите нужное в
+              поиске
+            </p>
+            <button>
+              <Link href="/">Перейти на главную</Link>
+            </button>
+          </div>
+        ) : (
+          <>
+            <div className={scss.desc}>
+              <p>картинка</p>
+              <p>название</p>
+              <p>цена</p>
+              <p>количество</p>
+              <p>удалить</p>
+            </div>
+            <div className={scss.content}>
+              {basket.map((el) => (
+                <div key={el.id} className={scss.product}>
+                  <div className={scss.box_img}>
+                    {el.photos?.map(
+                      (item, index) =>
+                        index === 0 && (
+                          <Image
+                            key={index}
+                            width={250}
+                            height={200}
+                            src={item.image}
+                            alt="img"
+                          />
+                        )
+                    )}
+                  </div>
+                  <div className={scss.details}>
+                    <h2 className={scss.model}>Модель: {el.model}</h2>
+                    <h3 className={scss.price}>
+                      Цена: {Number(el.price) * Number(el.quantity)} сом
+                    </h3>
+                    <div className={scss.quantity}>
+                      <button
+                        onClick={() => deleteOneBasket(el)}
+                        className={scss.decrease}
+                      >
+                        -
+                      </button>
+                      <span>{el.quantity}</span>
+                      <button
+                        onClick={() => addToBasket(el)}
+                        className={scss.increase}
+                      >
+                        +
+                      </button>
+                    </div>
 
-										<button
-											onClick={() => removeFromBasket(el)}
-											className={scss.removeButton}>
-											Удалить
-										</button>
-									</div>
-								</div>
-							))}
-							<form onSubmit={handleSubmit(onSubmit)} className={scss.form}>
-								 
-									<input placeholder="Телефон" type="tel" {...register("phone")} required />
-								 
-									<input placeholder="Email" type="email" {...register("email")} required />
-								<button type="submit">Отправить заказ</button>
-							</form>
-						</div>
-					</>
-				)}
-			</div>
-		</div>
-	);
+                    <button
+                      onClick={() => removeFromBasket(el)}
+                      className={scss.removeButton}
+                    >
+                      Удалить
+                    </button>
+                  </div>
+                </div>
+              ))}
+              <form onSubmit={handleSubmit(onSubmit)} className={scss.form}>
+                <input
+                  placeholder="Телефон"
+                  type="tel"
+                  {...register("phone")}
+                  required
+                />
+                <input
+                  placeholder="Email"
+                  type="email"
+                  {...register("email")}
+                  required
+                />
+                <button type="submit">Отправить заказ</button>
+              </form>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Basket;
--- src/appPages/components/pages/home/sections/laptops/Laptops.tsx
+++ src/appPages/components/pages/home/sections/laptops/Laptops.tsx
@@ -102,100 +102,112 @@ import Link from "next/link";
 import useBasketStore from "@/appPages/stores/useBasketStore";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { useFilterStore } from "@/appPages/stores/sortStore";
 
 const Laptops = () => {
-	const { data, isLoading } = useGetLaptopsQuery();
-	const [visibleCount, setVisibleCount] = useState(10);
-	const [showAll, setShowAll] = useState(false);
+  const { data, isLoading } = useGetLaptopsQuery();
+  const [visibleCount, setVisibleCount] = useState(10);
+  const [showAll, setShowAll] = useState(false);
 
-	useEffect(() => {
-		AOS.init({ duration: 1000 });
-	}, []);
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
 
-	const { addToBasket } = useBasketStore();
+  const { addToBasket } = useBasketStore();
 
-	const handleShowMore = () => {
-		if (!showAll) {
-			setVisibleCount(data?.length || 0);
-			setShowAll(true);
-		} else {
-			setVisibleCount(10);
-			setShowAll(false);
-		}
-	};
+  const handleShowMore = () => {
+    if (!showAll) {
+      setVisibleCount(data?.length || 0);
+      setShowAll(true);
+    } else {
+      setVisibleCount(10);
+      setShowAll(false);
+    }
+  };
 
-	if (isLoading) {
-		return (
-			<div id={scss.Laptops}>
-				<div className="container">
-					<div className={scss.laptops}>
-						<div className={scss.block}>
-							{[...Array(10)].map((_, index) => (
-								<div key={index} className={`${scss.box} ${scss.skeleton}`}>
-									<div className={scss.box_img}></div>
-									<div className={scss.box_text}>
-										<div className={scss.skeleton_title}></div>
-										<div className={scss.skeleton_price}></div>
-										<div className={scss.skeleton_buttons}></div>
-									</div>
-								</div>
-							))}
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  const { selectedType, selectedCategory } = useFilterStore();
 
-	return (
-		<div id={scss.Laptops}>
-			<div className="container">
-				<div className={scss.laptops}>
-					<div className={scss.block}>
-						{data?.slice(0, visibleCount).map((el, index) => (
-							<div
-								data-aos="fade-up"
-								data-aos-delay={index * 100}
-								key={el.id}
-								className={scss.box}>
-								<div className={scss.box_img}>
-									{el.photos.map(
-										(item, photoIndex) =>
-											photoIndex === 0 && (
-												<Image
-													width={220}
-													height={250}
-													key={photoIndex}
-													src={item.image}
-													alt="img"
-												/>
-											)
-									)}
-								</div>
-								<div className={scss.box_text}>
-									<h1>{el.model}</h1>
-									<div className={scss.price}>
-										<h2>{el.price} сом</h2>
-									</div>
-									<div className={scss.buttons}>
-										<Link href={`/details/${el.id}`}>
-											<button>Подробнее</button>
-										</Link>
-										<button onClick={() => addToBasket(el)}>В корзину</button>
-									</div>
-								</div>
-							</div>
-						))}
-					</div>
-					{data && data.length > 10 && (
-						<button className={scss.More} onClick={handleShowMore}>
-							{showAll ? "СВЕРНУТЬ" : "ЕЩЕ"}
-						</button>
-					)}
-				</div>
-			</div>
-		</div>
-	);
+  const filteredData = data?.filter((el) => {
+    const matchesType = selectedType ? el.brand === selectedType : true;
+    const matchesCategory = selectedCategory
+      ? el.brand === selectedCategory
+      : true;
+    return matchesType && matchesCategory;
+  });
+
+  if (isLoading) {
+    return (
+      <div id={scss.Laptops}>
+        <div className="container">
+          <div className={scss.laptops}>
+            <div className={scss.block}>
+              {[...Array(10)].map((_, index) => (
+                <div key={index} className={`${scss.box} ${scss.skeleton}`}>
+                  <div className={scss.box_img}></div>
+                  <div className={scss.box_text}>
+                    <div className={scss.skeleton_title}></div>
+                    <div className={scss.skeleton_price}></div>
+                    <div className={scss.skeleton_buttons}></div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div id={scss.Laptops}>
+      <div className="container">
+        <div className={scss.laptops}>
+          <div className={scss.block}>
+            {filteredData?.slice(0, visibleCount).map((el, index) => (
+              <div
+                data-aos="fade-up"
+                data-aos-delay={index * 100}
+                key={el.id}
+                className={scss.box}
+              >
+                <div className={scss.box_img}>
+                  {el.photos.map(
+                    (item, photoIndex) =>
+                      photoIndex === 0 && (
+                        <Image
+                          width={220}
+                          height={250}
+                          key={photoIndex}
+                          src={item.image}
+                          alt="img"
+                        />
+                      )
+                  )}
+                </div>
+                <div className={scss.box_text}>
+                  <h1>{el.model}</h1>
+                  <div className={scss.price}>
+                    <h2>{el.price} сом</h2>
+                  </div>
+                  <div className={scss.buttons}>
+                    <Link href={`/details/${el.id}`}>
+                      <button>Подробнее</button>
+                    </Link>
+                    <button onClick={() => addToBasket(el)}>В корзину</button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          {data && data.length > 10 && (
+            <button className={scss.More} onClick={handleShowMore}>
+              {showAll ? "СВЕРНУТЬ" : "ЕЩЕ"}
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Laptops;
